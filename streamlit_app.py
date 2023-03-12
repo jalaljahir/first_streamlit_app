74e495bd8e30c071a9d906b3de8b9b84ce31a912
Jalal Jahir <jalaljahir@users.noreply.github.com>
2023-03-12 06:50:52 -0500

    commented out stop streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ except URLError as e:
 
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+# streamlit.stop()
 
 streamlit.header("The fruit load list contains: ")
 #snowflake-related func
